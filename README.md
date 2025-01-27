f7978b3c0d4c7ec21ec9e72947e29e82945ad0f2
Chloe Prime <chloe_von_einzbern@qq.com>
2025-01-27 14:48:20 +0800

    Port to NeoForge 1.21

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# About Forge PB4 Placeholder API
+This is a port of [Patbox's Placeholder API on Fabric](https://github.com/Patbox/TextPlaceholderAPI)  
+Don't report issues to the original repo unless it can be reproduced on the original Fabric version!
+
 # About Placeholder API
 It's a small, jij-able API that allows creation and parsing placeholders within strings and Minecraft Text Components.
 Placeholders use simple format of `%modid:type%` or `%modid:type/data%`.
--- src/main/java/eu/pb4/placeholders/impl/textparser/BuiltinTags.java
+++ src/main/java/eu/pb4/placeholders/impl/textparser/BuiltinTags.java
@@ -148,7 +148,7 @@ public final class BuiltinTags {
                                 textList.add(parser.parseNode(part));
                             }
 
-                            return TranslatedNode.ofFallback(key, fallback, textList.toArray(TextParserImpl.CASTER));
+                            return TranslatedNode.ofFallback(key, fallback, (Object[]) textList.toArray(TextParserImpl.CASTER));
                         }
                         return TextNode.empty();
                     })
@@ -176,7 +176,7 @@ public final class BuiltinTags {
                                 textList.add(parser.parseNode(part));
                             }
 
-                            return TranslatedNode.ofFallback(key, fallback, textList.toArray(TextParserImpl.CASTER));
+                            return TranslatedNode.ofFallback(key, fallback, (Object[]) textList.toArray(TextParserImpl.CASTER));
                         }
                         return TextNode.empty();
                     })
